5352f8a589393daf7b493107648d0fd95957ee2b
Melon_NG <30720036+kyuranger@users.noreply.github.com>
2023-05-16 04:31:46 +0800

    Update file

---
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using Avalonia.Styling;
+using System;
+using System.IO;
+
+namespace AvaloniaApplication1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+  
+    }
+}
---
+++ AvaloniaApplication1/TemplatedControl1.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+
+namespace AvaloniaApplication1;
+
+public class TemplatedControl1 : ToggleButton
+{
+  
+}
